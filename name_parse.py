663bd83a9fbd5891a6afd6653cde5f5979289a17
Olivia Chen <oliviachen56@gmail.com>
2016-07-08 14:22:58 -0700

    fix bug and increse auc

--- name_parse.py
+++ name_parse.py
@@ -9,47 +9,6 @@ class NameParse(object):
         self.parsed_name = None
         self._parse()
 
-    # def process_content(name):
-    #     name = name.replace('. ', '.')
-    #     tokenized = custom_sent_tokenize.tokenize(name)
-    #     for i in tokenized:
-    #         words = nltk.word_tokenize(i)
-    #         jobtil = list(set(words)&set(title))
-    #         if len(jobtil) != 0:
-    #             if ',' in words:
-    #                 if words.index(jobtil[0]) < words.index(','):
-    #                     newstr = name.split(',')[1]
-    #                     print newstr
-    #                 else:
-    #                     newstr = name.split(',')[0]
-    #                     print newstr
-    #             else:
-    #                 if words.index(jobtil[0]) == len(words)-1:
-    #                         newstr = words[:-2]
-    #                 else:
-    #                     newstr = words[words.index(jobtil[0])+1:]
-    #                     print newstr
-    #             parsed_name = pp.parse(newstr)
-    #             for i in parsed_name:
-    #                 if i[1] == 'Surname':
-    #                     last_name = i[0].strip(',')
-    #                 elif i[1] == 'GivenName':
-    #                     first_name = i[0]
-    #                 else:
-    #                     pass
-    #             self.parsed_name = (name.first, name.last)
-    #
-    #         else:
-    #             parsed_name = pp.parse(name)
-    #             for i in parsed_name:
-    #                 if i[1] == 'Surname':
-    #                     last_name = i[0].strip(',')
-    #                 elif i[1] == 'GivenName':
-    #                     first_name = i[0]
-    #                 else:
-    #                     pass
-    #             self.parsed_name = (name.first, name.last)
-
     def _parse(self):
         '''
         Assign the first and last name in self.name to self.parsed_name
@@ -67,16 +26,13 @@ class NameParse(object):
                 if ',' in words:
                     if words.index(jobtil[0]) < words.index(','):
                         newstr = name.split(',')[1]
-                        print newstr
                     else:
                         newstr = name.split(',')[0]
-                        print newstr
                 else:
                     if words.index(jobtil[0]) == len(words)-1:
-                            newstr = words[:-2]
+                            newstr = ' '.join(words[:-2])
                     else:
-                        newstr = words[words.index(jobtil[0])+1:]
-                        print newstr
+                        newstr = ' '.join(words[words.index(jobtil[0])+1:])
                 parsed_name = pp.parse(newstr)
                 for i in parsed_name:
                     if i[1] == 'Surname':
@@ -85,7 +41,7 @@ class NameParse(object):
                         first_name = i[0]
                     else:
                         pass
-                self.parsed_name = (first_name, last_name)
+                self.parsed_name = (first_name, last_name.replace('.', '. '))
 
             else:
                 parsed_name = pp.parse(name)
@@ -96,4 +52,4 @@ class NameParse(object):
                         first_name = i[0]
                     else:
                         pass
-                self.parsed_name = (first_name, last_name)
+                self.parsed_name = (first_name, last_name.replace('.', '. '))
